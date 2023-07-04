a38ea66506235b989eddb2e60c603b8046ed98b4
Rodrigo César <rodrigocnn@gmail.com>
2023-07-04 17:19:16 -0300

    config port env settting.py

--- clinica/settings.py
+++ clinica/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'USER':  os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASS'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': 3306,
+        'PORT': os.getenv('DB_PORT'),
 
     }
 }
